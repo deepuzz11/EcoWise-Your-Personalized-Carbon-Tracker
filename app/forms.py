1aa9ae336ec6caddc0411680b2a09b0b99565891
Deepika P <deepikaprabhakaran11@gmail.com>
2024-11-26 19:53:15 +0530

    removed unnecessary files

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class UserForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    submit = SubmitField('Add User')
--- app/utils.py
+++
@@ -1,4 +0,0 @@
-def calculate_carbon_footprint(activities):
-    # Example function to calculate carbon footprint based on user activities
-    footprint = sum(activity['emissions'] for activity in activities)
-    return footprint
